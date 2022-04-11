1c22750570517a1ed57d1cf0d63b50e4270c29f4
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-11 11:31:37 -0500

    Navbar addition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React from "react";
+import Nav from "./components/Nav";
+
+
 function App(){
 
-  return <h1> Hello World</h1>
+  return (
+    <div>
+      <Nav />
+      <h1> Hello World</h1>
+     </div>
+  )
 }
 
 export default App;
