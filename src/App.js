a2c196cd73027b7da06a8ef3b5c76fcd5d8eb025
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-11 17:28:24 -0500

    render mount and construct

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import Nav from "./components/Nav";
 export default class App extends Component{
     constructor(props){
         super(props);
+        console.log('App constructed')
         this.state = {
             count: 0
         }
+    }
+
+    componentDidMount(){
+        console.log('App mounted')
     }
 
     handleButtonClick = (step) => {
@@ -20,13 +25,14 @@ export default class App extends Component{
 
     render(){
         const myButtonSteps = [1, 10, 100, 1000, 10000, 100000, 1000000]
+        console.log('App rendered')
         return (
             <>
                 <Nav />
                 <div className='container'>
                     <h1>Hello World!</h1>
                     <h4 className='text-center'>Current Count: {this.state.count}</h4>
-                    {myButtonSteps.map(step => <Button step={step} handleClick={this.handleButtonClick} />)}
+                    {myButtonSteps.map((step, i) => <Button key={i} step={step} handleClick={this.handleButtonClick} />)}
     
                 </div>
             </>
--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,6 @@ export default class Button extends Component {
 
 
     render() {
-        console.log('Rendered')
         return (
             <button onClick={() => this.props.handleClick(this.props.step)} className='btn btn-primary d-block w-100'>Step: {this.props.step}</button>
         )
