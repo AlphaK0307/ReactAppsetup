4f462983409921ed82cb00352b1106479f0e9ddf
AlphaK0307 <keyurpatel1121@gmail.com>
2022-04-11 13:19:23 -0500

    buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Component } from 'react';
+import Button from './components/Button';
 import Nav from "./components/Nav";
 
 
-function App(){
+export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
 
-  return (
-    <div>
-      <Nav />
-      <h1> Hello World</h1>
-     </div>
-  )
+    handleButtonClick = (step) => {
+        let newCount = this.state.count + step;
+        this.setState({
+            count: newCount
+        })
+    }
+
+    render(){
+        const myButtonSteps = [1, 10, 100, 1000, 10000, 100000, 1000000]
+        return (
+            <>
+                <Nav />
+                <div className='container'>
+                    <h1>Hello World!</h1>
+                    <h4 className='text-center'>Current Count: {this.state.count}</h4>
+                    {myButtonSteps.map(step => <Button step={step} handleClick={this.handleButtonClick} />)}
+    
+                </div>
+            </>
+        )
+    }
 }
-
-export default App;
